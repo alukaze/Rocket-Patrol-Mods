e19079d86f127cb10c8b7d8aac90374ac857f1ae
Alan Lu <alu36@ucsc.edu>
2024-01-31 12:04:18 -0800

    Complete

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+//Name: Alan Lu
+//Title: Rocket Patrol Winter DLC: Interstellar
+//Time Worked: 8
+//Mods: 
+// Create a new scrolling tile sprite for the background (1 point)
+// Create 4 new explosion effects and randomize which one plays on impact (3 points)
+// Display the time remaining in seconds on the screen (3 points)
+// Implement parallax scrolling for the background (3 points)
+// Create a new enemy spaceship type (w/ new artwork) thats smaller, moves faster, and is worth more points (5 points)
+// Implement a new timing/scoring mechanism that adds time for successful hits and subtracts time on misses (5 points)
+
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
